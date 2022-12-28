3a76e30f008a75553cf7721d733e5945535d1f1c
Simone Lungarella <simonelungarella@gmail.com>
2022-12-28 09:10:42 +0100

    Updates

--- output/server/nodes/8.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 8;
-export const component = async () => (await import('../entries/pages/projects/winforce/_page.svelte.js')).default;
-export const file = '_app/immutable/components/pages/projects/winforce/_page.svelte-37356b68.js';
-export const imports = ["_app/immutable/components/pages/projects/winforce/_page.svelte-37356b68.js","_app/immutable/chunks/index-86cd6b2a.js"];
-export const stylesheets = [];
-export const fonts = [];
