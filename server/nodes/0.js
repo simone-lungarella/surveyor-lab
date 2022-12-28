3347dc0c88b730daedbbebee330408182da24d61
Simone Lungarella <simonelungarella@gmail.com>
2022-12-28 09:22:51 +0100

    Updates

--- server/nodes/0.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 0;
-export const component = async () => (await import('../entries/pages/_layout.svelte.js')).default;
-export const file = '_app/immutable/components/pages/_layout.svelte-3fd96149.js';
-export const imports = ["_app/immutable/components/pages/_layout.svelte-3fd96149.js","_app/immutable/chunks/index-86cd6b2a.js"];
-export const stylesheets = ["_app/immutable/assets/_layout-bf535cd1.css"];
-export const fonts = [];
