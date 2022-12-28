e3d637a00e167d06f4c23de26a8aacdf2fdacbca
Simone Lungarella <simonelungarella@gmail.com>
2022-12-28 09:21:10 +0100

    Updates

--- server/index.js
+++ server/index.js
@@ -2412,7 +2412,7 @@ class Server {
       app_template,
       app_template_contains_nonce: false,
       error_template,
-      version: "1672214996323"
+      version: "1672215660601"
     };
   }
   async init({ env }) {
