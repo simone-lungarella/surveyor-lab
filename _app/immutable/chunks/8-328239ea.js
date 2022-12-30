1a3b993db3f72d056db58cf886f54b6d56aa4269
simone-lungarella <simone-lungarella@users.noreply.github.com>
2022-12-30 16:59:25 +0000

    deploy: 1428da5b18ddf77586104e8d9e006a2373e82a6c

--- _app/immutable/chunks/8-328239ea.js
+++ _app/immutable/chunks/8-328239ea.js
@@ -1,1 +1,1 @@
-import{_ as r}from"./_page-fca24c55.js";import{default as t}from"../components/pages/projects/winforce/_page.svelte-37356b68.js";export{t as component,r as universal};
+import{_ as r}from"./_page-b95cbc4a.js";import{default as t}from"../components/pages/projects/winforce/_page.svelte-37356b68.js";export{t as component,r as universal};
