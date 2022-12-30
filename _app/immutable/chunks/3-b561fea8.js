f93cfb86798dcaba3a0c28c044c4bcb7bc742aae
Simone Lungarella <simonelungarella@gmail.com>
2022-12-30 12:34:13 +0100

    Updates

--- _app/immutable/chunks/3-b561fea8.js
+++ _app/immutable/chunks/3-b561fea8.js
@@ -1,1 +1,1 @@
-import{_ as r}from"./_page-7c799ff2.js";import{default as t}from"../components/pages/about/_page.svelte-ae2f6497.js";export{t as component,r as universal};
+import{_ as r}from"./_page-da46b06b.js";import{default as t}from"../components/pages/about/_page.svelte-ae2f6497.js";export{t as component,r as universal};
