f79e6b8bc638e9642096ecc9ce3cf56ba2f6df9a
simone-lungarella <simone-lungarella@users.noreply.github.com>
2022-12-31 11:34:58 +0000

    deploy: 61e51021ed91c8bf56d3f151086c2a566c17e9d9

--- _app/immutable/chunks/4-69f9a92d.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./_page-7c799ff2.js";import{default as t}from"../components/pages/experience/_page.svelte-152ad109.js";export{t as component,r as universal};
---
+++ _app/immutable/chunks/5-d580bc6e.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./_page-2a9d3cfa.js";import{default as t}from"../components/pages/projects/_page.svelte-7c6f73ab.js";export{t as component,r as universal};
